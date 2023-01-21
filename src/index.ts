b84bf910b7ed14679d8648b2f5204826a14f9f11
smiilliin <smiilliindeveloper@gmail.com>
2023-01-21 12:25:59 +0900

    Deleted example

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import Settings from "./settings";
-
-const settings = new Settings("test-app");
-
-console.log(settings.load("settings.json"));
-// settings.set("settings.json", {
-//   test: 1,
-// });
